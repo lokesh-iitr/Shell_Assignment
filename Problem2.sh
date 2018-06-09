bfe5232c6c205c74baf202c9bbc29ce2e03074e3
lokesh-iitr <lokesh.mahala@quantiphi.com>
2018-06-10 01:53:22 +0530

    Small change in Assign

--- Problem2.sh
+++ Problem2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Create our first instance and upload a file into s3 bucket 
 aws ec2 run-instances --image-id ami-14c5486b --count 1 --instance-type t2.micro --key-name shellscripting --security-group-ids --iam-instance-profile Name=PE_trainee_Admin_role --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Shell-assignment}, {Key=username,Value=Lokesh_Mahala},{Key=email_id,Value=lokesh.mahala@quantiphi.com}, {Key=Project,Value=PE_Training}]' --query Instances[0].InstanceId --user-data '#!/bin/bash 
-echo "Hello Quantiphi This is final solution of my Shell Assignment schhi mai" $
+echo "Hello Quantiphi This is final solution of my Shell Assignment schhi mai" 
 aws s3 cp finalfile1.txt s3://2017lokesh/finalfile1.txt' 
 
 #check our file present in s3 bucket or not 
